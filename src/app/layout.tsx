7230736ac17205d648ecd73538ea4074a01a388a
RutPatelSCIT <92986759+RutPatelSCIT@users.noreply.github.com>
2025-09-17 23:24:28 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'AdminBot',
-  description: 'An AI Chatbot for Admins',
+  title: 'Apnastocks.in',
+  description: 'An AI Chatbot for investors',
 };
 
 export default function RootLayout({
