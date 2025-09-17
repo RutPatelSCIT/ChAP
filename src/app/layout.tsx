dd5ab9ad369c6ea11238a8cf4dccdea08e18e025
RutPatelSCIT <92986759+RutPatelSCIT@users.noreply.github.com>
2025-09-17 23:25:56 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Apnastocks.in',
+  title: 'Adminbot',
   description: 'An AI Chatbot for investors',
 };
 
